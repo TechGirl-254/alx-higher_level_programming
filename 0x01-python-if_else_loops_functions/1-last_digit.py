b927b56c4782ac9e6c827decdb0d91e240a904dc
TechGirl-254 <wammymuriu@gmail.com>
2022-09-06 05:25:52 -0700

    Editing the py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-if number < 10:
-    if number > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, number))
-    elif number == 0:
-        print("Last digit of {} is {} and is 0".format(number, number))
-    elif number < 6 and number != 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number,
-    number))
-if number > 10 and number < 10000:
+number = int(input("Enter a number between -10000 and 10000 : "))
+if number < 0 and number > -10000:
+    print("Last digit of {} is -{} and is less than 6 and not 0".format(number,
+    (10- (number % 10))))
+if number > 0 and number < 10000:
     if number % 10 > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, number))
-    elif number % 10 == 0:
-        print("Last digit of {} is {} and is 0".format(number, number))
+        print("Last digit of {} is {} and is greater than 5".format(number,
+            number % 10))
     elif number % 10 < 6 and number % 10 != 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number,
-        number))
+        print("Last digit of {} is {} and is less than 6 andnot 0".format(number, number % 10))
+    elif number % 10 == 0:
+        print("Last digit of {} is {} and is 0".format(number, number % 10))
