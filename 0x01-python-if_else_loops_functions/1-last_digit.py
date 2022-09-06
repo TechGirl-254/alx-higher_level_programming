ac717cfdf1360c14c2488d7e0b4b7713562d591b
TechGirl-254 <wammymuriu@gmail.com>
2022-09-06 05:28:09 -0700

    Editing the py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-number = int(input("Enter a number between -10000 and 10000 : "))
+import random
+number = random.randint(-10000, 10000)
 if number < 0 and number > -10000:
     print("Last digit of {} is -{} and is less than 6 and not 0".format(number,
     (10- (number % 10))))
