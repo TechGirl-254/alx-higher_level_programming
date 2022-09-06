bdbbb7127b272ad955e59b891f00d05c18b2ff07
TechGirl-254 <wammymuriu@gmail.com>
2022-09-06 06:13:39 -0700

    Editing the py file

--- 0x01-python-if_else_loops_functions/correct.py
+++ 0x01-python-if_else_loops_functions/correct.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import math
-import random
-number = random.randint(-10000, 10000)
+number = int(input("Enter a number bewteen -10000 and 10000: "))
 if number < 0 and number > -10000:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number,
         (math.trunc(math.fmod(number, 10)))))
