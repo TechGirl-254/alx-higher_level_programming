46774702a59df4e5fb676614b4aa47ef05d72677
TechGirl-254 <wammymuriu@gmail.com>
2022-09-06 20:28:19 -0700

    Editing the py file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 i = 0
-while(i <= 99):
-    for i in range(100):
+while(i <= 98):
+    for i in range(99):
         print("{},".format(i), end=' ')
-    print()
+    print("99")
     i += 1
