6090339db494a0b77707880e395503ae7ee1eafc
TechGirl-254 <wammymuriu@gmail.com>
2022-09-06 20:05:28 -0700

    Editing the py file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -4,6 +4,5 @@ i = 0
 
 while(i <= 90):
     for i in range(97, 123):
-        print("{}".format(chr(i)), end = '')
+        print("{}".format(chr(i)),end='')
         i += 1
-
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -5,5 +5,5 @@ i = 0
 while(i <= 122):
     for i in range(97, 123):
         if i != 113 and i != 101:
-            print("{}".format(chr(i)), end = '')
+            print("{}".format(chr(i)),end='')
         i += 1
