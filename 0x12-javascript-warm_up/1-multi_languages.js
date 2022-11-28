30599961668f9253c3382e86d06607adddbcab0e
TechGirl-254 <wammymuriu@gmail.com>
2022-11-28 14:32:56 -0800

    Adding text using JS

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 let cProg = "C is fun";
 let pyProg = "Python is cool";
-let jProg = "JavaScript is cool";
-console.log(cProg,'\n',pyProg,'\n',jProg,'\n');
+let jProg = "JavaScript is amazing";
+console.log('',cProg,'\n',pyProg,'\n',jProg,'\n');
