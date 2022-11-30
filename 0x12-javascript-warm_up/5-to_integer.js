3377dbe87f775d29ff5fbafe05e186c2acfae603
TechGirl-254 <105039690+TechGirl-254@users.noreply.github.com>
2022-12-01 01:28:26 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
-const myArg = process.argv;
 const myArg2 = process.argv[2];
 if (!myArg2) {
   console.log('Not a number');
-}
-let i = 0;
-while (myArg[i]) {
-  else {
-    if (isNaN(myArg[i])) {
+} else {
+    if (isNaN(myArg2)) {
       console.log('Not a number');
     } else {
       console.log('My number:', myArg[i]);
     }
   }
-  i++;
-}
