a43c81911ed2ba1b71433f6b3ba8cb9d02839962
TechGirl-254 <wammymuriu@gmail.com>
2022-11-30 06:33:59 -0800

    Making changes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let len = process.argv;
-if (len.length < 3){
-	console.log("No argument");
-} else if (len.length === 3){
-	console.log("Argument found");
-} else{
-	console.log("Arguments found");
+const len = process.argv;
+if (len.length < 3) {
+  console.log('No argument');
+} else if (len.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-console.log("\n");
+console.log('\n');
