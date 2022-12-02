19213380feb937481b78faf4acd4f65a7ab6aba6
TechGirl-254 <wammymuriu@gmail.com>
2022-12-02 12:22:04 -0800

    Accessing and manipulating CL arguements

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,9 +3,9 @@ const myArg2 = process.argv[2];
 if (!myArg2) {
   console.log('Not a number');
 } else {
-    if (isNaN(myArg2)) {
-      console.log('Not a number');
-    } else {
-      console.log('My number:', myArg2);
-    }
+  if (isNaN(myArg2)) {
+    console.log('Not a number');
+  } else {
+    console.log('My number:', myArg2);
   }
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const x = process.argv[2];
+const y = isNaN(x);
+let i = 1;
+if (!x) {
+  console.log('Missing number of occurrences');
+} else {
+  if (y) {
+    console.log('Missing number of occurrences');
+  } else {
+    while (i <= x) {
+      console.log('C is fun');
+      i++;
+    }
+  }
+}
