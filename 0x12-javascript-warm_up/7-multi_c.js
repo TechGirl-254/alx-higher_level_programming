3e0bbf25fb28e1b15e5acbc382a91610ac6fccf2
TechGirl-254 <105039690+TechGirl-254@users.noreply.github.com>
2022-12-02 23:25:49 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,15 +2,11 @@
 const x = process.argv[2];
 const y = isNaN(x);
 let i = 1;
-if (!x) {
+if (!x || y) {
   console.log('Missing number of occurrences');
 } else {
-  if (y) {
-    console.log('Missing number of occurrences');
-  } else {
     while (i <= x) {
       console.log('C is fun');
       i++;
     }
-  }
 }
