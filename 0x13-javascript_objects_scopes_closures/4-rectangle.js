4ae9ad1a739b198f88cbb71d5d5f83d2dad93864
TechGirl-254 <105039690+TechGirl-254@users.noreply.github.com>
2022-12-06 06:16:15 +0300

    Update 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+class Rectangle {
+constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+    print() {
+        let i = 1;
+        while (i <= this.height) {
+            console.log('X'.repeat(this.width));
+            i++;
+        }
+    }
+    rotate() {
+        let k = this.width;
+        this.width = this.height
+        this.height = k;
+    }
+    double() {
+        this.width *= 2;
+        this.height *= 2;
+    }
+}
+module.exports = Rectangle;  
