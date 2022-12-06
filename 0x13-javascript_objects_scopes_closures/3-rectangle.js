dcee2c7f28d9146216d9daaf2e9e93e188f119e7
TechGirl-254 <wammymuriu@gmail.com>
2022-12-05 19:03:41 -0800

    Printing a rectangle using a class

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    print() {
-        let i = 1;
-        while (i <= this.height) {
-            console.log('X'.repeat(this.width));
-            i++;
-        }
+  }
+
+  print () {
+    let i = 1;
+    while (i <= this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
     }
+  }
 }
 module.exports = Rectangle;
