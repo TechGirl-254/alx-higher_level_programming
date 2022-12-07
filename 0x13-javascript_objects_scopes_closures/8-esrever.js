63ac7c9ea92024dd43048ff68d1149fefb43e96c
TechGirl-254 <wammymuriu@gmail.com>
2022-12-07 03:23:12 -0800

    Reversing lists

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const listA = [];
-    let i = (list.length - 1);
-    let j = 0;
-    while (i >= 0) {
-        listA[j] = list[i];
-        i--;
-        j++;
-    }
-    return (listA);
-}
+  const listA = [];
+  let i = (list.length - 1);
+  let j = 0;
+  while (i >= 0) {
+    listA[j] = list[i];
+    i--;
+    j++;
+  }
+  return (listA);
+};
