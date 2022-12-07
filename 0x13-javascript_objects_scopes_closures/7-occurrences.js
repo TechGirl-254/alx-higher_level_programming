3433c2234966debb61f5c24b4f4f442f5fc6ba60
TechGirl-254 <105039690+TechGirl-254@users.noreply.github.com>
2022-12-07 13:59:00 +0300

    Update 7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+    let i = 0;
+    let j = 0;
+    while (j < (list.length)) {
+        if (list[j] === searchElement) {
+            i++;
+        }
+        j++;
+    }
+    return (i);
+}
