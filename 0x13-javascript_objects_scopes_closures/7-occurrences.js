4b5b5c9ebede99889b2be0e7d3b33e03b1218bc4
TechGirl-254 <wammymuriu@gmail.com>
2022-12-07 03:00:16 -0800

    Calculating occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let i = 0;
-    let j = 0;
-    while (j < (list.length)) {
-        if (list[j] === searchElement) {
-            i++;
-        }
-        j++;
+  let i = 0;
+  let j = 0;
+  while (j < (list.length)) {
+    if (list[j] === searchElement) {
+      i++;
     }
-    return (i);
-}
+    j++;
+  }
+  return (i);
+};
