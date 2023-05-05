b2b486d0cf1c91121f75efe6eab403108ac27435
TechGirl-254 <wammymuriu@gmail.com>
2023-05-05 13:04:12 +0300

    Making a post request and including header variables

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sending a post request with custom header variables
-curl -s "$1" -X "POST" -H "email: test@gmail.com", "subject: I will always be here for PLD"
+curl -s "$1" -X "POST" -d "email=test%40gmail%2Ecom&subject=I+will+always+be+here+for+PLD"
