e0f556912bbf8d705e9c19d3244301b34b252afe
TechGirl-254 <wammymuriu@gmail.com>
2023-05-09 10:25:43 +0300

    Editing the code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
-"""Sending a GET request using request package"""
+""" Module to fetch https://intranet.hbtn.io/status
+    with requests module
+"""
 
 import requests
 
+
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
-    response = r.text
-    print(f"Body response:\n\t- type:{type(response)}\n\t- content:{response}")
+    response = requests.get("https://intranet.hbtn.io/status").text
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+          .format(type(response), response))
