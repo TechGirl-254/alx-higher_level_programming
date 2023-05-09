779359436a4e06efb5757ce2344823858f91adb2
TechGirl-254 <wammymuriu@gmail.com>
2023-05-09 06:47:59 +0300

    Editing the code

--- 0x10-python-network_0/0x11-python-network_1/0-hbtn_status.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import urllib.request as u
-
-with u.urlopen('https://alx-intranet.hbtn.io/status') as f:
-    data = f.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(data), data, data.decode("utf-8")))
--- 0x10-python-network_0/0x11-python-network_1/README.md
+++
@@ -1,1 +0,0 @@
-This file contains steps on how to get resources from the internet programatically
