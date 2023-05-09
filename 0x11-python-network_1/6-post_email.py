e81e749cea8d82975029f63348afe779fdf6a19a
TechGirl-254 <wammymuriu@gmail.com>
2023-05-09 07:44:59 +0300

    Editing the code

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,9 +6,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = "argv[1]"
     mydata = {
             "email": "argv[2]"
             }
-    page = requests.post(url, data=mydata)
+    page = requests.post(argv[1], data=mydata)
     print(f"Your email is: {page.text}")
